6041fc1d5f5b6a0c51d3e28ec3387fac2591db6b
George Stoyanov <gstoyanov@gmail.com>
2017-09-01 14:05:16 +0200

    Update opencaster.sh

--- opencaster.sh
+++ opencaster.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-sudo update && sudo apt upgrade -y && sudo apt install wget -y
+echo "Update and installation of the required packages"
 
+sudo update
+sudo apt upgrade -y 
+
+sudo apt install wget \
+binutils \
+gcc \
+libc6-dev \
+libgomp1 \
+linux-libc-dev \
+make \
+python-dev \
+zlib1g-dev \
+python-dateutil -y
+
+echo "Download and extraction of the Opencaster Software"
+wget http://ftp.de.debian.org/debian/pool/main/o/opencaster/opencaster_3.2.2+dfsg.orig.tar.gz
 tar xvf opencaster_3.2.2+dfsg.orig.tar.gz
 cd opencaster-3.2.2/
 
-sudo apt install binutils gcc libc6-dev libgomp1 linux-libc-dev make python-dev zlib1g-dev python-dateutil -y
-
-# Replaces net on line 15 with linux to fix a problem in the compilation of the program
+echo "Replaces net on line 15 with linux to fix a problem in the compilation of the program"
 sed -i '15s/net/linux/' tools/mpe2sec/mpe.c
+
+echo "Compilation of the Opencaster package"
 
 make
 sudo make install
