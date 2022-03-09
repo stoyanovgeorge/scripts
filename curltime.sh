c659d507030e008be0bb7c5c36b6633870dce7c6
Georgi Stoyanov <gstoyanov@gmail.com>
2022-03-09 21:50:58 +0100

    update

--- curltime.sh
+++ curltime.sh
@@ -4,5 +4,5 @@
 if [ -z "$1" ];
 then echo "You have not defined a valid URL! 'curltime <URL>'"
 else
-    curl -L -w @"$PWD"/time_format.txt -o /dev/null -s "$1";
+    curl -L -w @"$HOME"/Documents/GitHub/scripts/curl_time_format.txt -o /dev/null -s "$1";
 fi
